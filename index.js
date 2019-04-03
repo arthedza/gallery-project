2a51f6e2a9aeba09fc59feff4f4d253f5eb6e415
Artur Zahorskyi <arthedza@gmail.com>
2019-04-03 17:03:53 +0300

    project

--- index.js
+++ index.js
@@ -2,17 +2,16 @@
 
 import imageCard from './modules/imageCard.js';
 customElements.define('image-card', imageCard);
+
 const sha1 = require('js-sha1');
-const EXIF = require('exif-js');
 
 const PORT = 3000;
 const contentWrapper = document.getElementsByClassName('content_wrapper')[0];
 const placeholder = document.getElementsByClassName('placeholder')[0];
 const btnRegister = document.getElementById('btn_register');
 const btnLogin = document.getElementById('btn_login');
-const email = document.getElementById('email'); //TODO: inputs verification
+const email = document.getElementById('email');
 const key = document.getElementById('password');
-
 
 function addElement(tagName, container) {
     return (container ? container : document.body).appendChild(document.createElement(tagName));
@@ -23,31 +22,21 @@ function getCurrentDate() {
 }
 
 function testFile(file) {
-    // debugger;
-    //TODO: new func
-    //TODO: resolve + reject //TODO: IF?
-
     return new Promise(() => {
         return /jpeg|png|gif/gi.test(file.type) ?
-            addElement('image-card', contentWrapper).setAttribute('src', URL.createObjectURL(file)) //TODO: separete function to insert image into the DOM
-            :
-            alert('Выбранный файл не является изображением'); //TODO: add img into web-component
+            addElement('image-card', contentWrapper).setAttribute('src', URL.createObjectURL(file)) :
+            alert('Выбранный файл не является изображением');
     });
 }
 
-//TODO: JSDOC /** */
-
 var selector = document.getElementById('image_selector');
 selector.onchange = function (event) {
-    // debugger
     for (var file of event.target.files) {
-        testFile(file).then(sendFile(file)); //TODO: get from coockies
-        // let metadata = getExif(file) //TODO: variable obj
+        testFile(file).then(sendFile(file));
     }
 };
-//TODO: rename funcs (function naming)
+
 async function sendFile(file) {
-    // debugger;
     fetch(`http://localhost:${PORT}/Images/`, {
             method: 'POST',
             headers: {
@@ -55,13 +44,12 @@ async function sendFile(file) {
             },
             body: JSON.stringify({
                 uploaded: getCurrentDate(),
-                ovner: getUserName(email.value), //TODO://TODO://TODO://TODO://TODO:
-                // metadata: getExif(file),
+                ovner: getUserName(email.value),
                 content: await encodeFile(file)
             })
         })
         .then((response) => response.json())
-        .then((response) => console.log(response));
+        .catch(error => console.log(error));
 }
 
 function encodeFile(file) {
@@ -73,7 +61,8 @@ function encodeFile(file) {
 }
 
 function getData(endpoint) {
-    return fetch(`http://localhost:${PORT}/${endpoint}/`).then((response) => response.json());
+    return fetch(`http://localhost:${PORT}/${endpoint}/`).then((response) => response.json())
+        .catch(error => console.log(error));
 }
 
 function showImg(img) {
@@ -107,21 +96,17 @@ function addNewUser(loginMail, key) {
         .then((response) => response.json())
         .then((response) => alert('registered'));
 }
-//TODO: async/await like here
+
 async function testUser(userName, key) {
     let response = await getData('Users');
-    // debugger
-    let user = await response.find((user) => user[userName]); //TODO: get data with username-endpoints//TODO://TODO://TODO://TODO://TODO://TODO://TODO://TODO://TODO://TODO://TODO://TODO:
-    // return await response[userName] ? true : false
-    //TODO: email verification: mail domains
-    // debugger
+    let user = await response.find((user) => user[userName]);
     if (!user) {
         alert('user does not exist');
-        location.reload(); //TODO:
+        location.reload();
     } else
         user[userName].name === userName && user[userName].key === key ?
-        beginSession(key) /*.bind(null, await user[userName], key)*/ :
-        console.log('lol');
+        beginSession(key) :
+        alert('error!');
 }
 
 function deleteCoockie(name) {
@@ -130,50 +115,25 @@ function deleteCoockie(name) {
 
 function beginSession(key) {
     console.log('hi');
-    let logOut = addElement('button', document.getElementsByClassName('topbar_wrapper')[0]); //TODO: make it const
+    let logOut = addElement('button', document.getElementsByClassName('topbar_wrapper')[0]);
     logOut.innerText = 'Logout';
     logOut.onclick = function (event) {
         alert('logout');
         deleteCoockie('key');
         location.reload();
     };
-    // debugger
-    // document.getElementById('image_selector').style.display = 'inline-block'
-    document.cookie = `key=${key};max-age=3600`; //TODO://TODO://TODO://TODO://TODO:
+    document.cookie = `key=${key};max-age=3600`;
     getData('Images').then((response) =>
         response.forEach((responseEntity) => {
-            // debugger
-            // placeholder.hidden = true;
-            responseEntity.ovner === getUserName(email.value) ? showImg(responseEntity) : placeholder.hidden = false; //null; TODO:
+            responseEntity.ovner === getUserName(email.value) ? showImg(responseEntity) : placeholder.hidden = false;
         })
-    ); //TODO: mb create custom response iterator
-
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    );
 }
 
-window.onload = function (event) {};
-
-btnRegister.onclick = (event) => addNewUser(email.value, sha1(key.value)); //TODO: inputs verification
+btnRegister.onclick = (event) => addNewUser(email.value, sha1(key.value));
 
 btnLogin.onclick = function (event) {
-    // debugger
     testUser(getUserName(email.value), sha1(key.value));
     document.getElementById('label').style.display = 'block';
     document.getElementById('auth').style.display = 'none';
 };
-
-//TODO:
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-function getExif(img) {
-
-
-    EXIF.getData(img, function () {
-        var allMetaData = EXIF.getAllTags(this);
-        var allMetaDataSpan = document.getElementById("allMetaDataSpan");
-        allMetaDataSpan.innerHTML = JSON.stringify(allMetaData, null, "\t");
-    });
-}
-
-// }
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
--- modules/imageCard.js
+++ modules/imageCard.js
@@ -3,21 +3,15 @@ const EXIF = require('exif-js');
 class imageCard extends HTMLElement {
     constructor() {
         super();
-        // let text;
 
         let wrapper = document.createElement('div');
         wrapper.className = 'wrapper';
         this.picture = document.createElement('img');
         this.picture.className = 'image';
         wrapper.appendChild(this.picture);
-        // this.date = document.createElement('span');
-        // this.date.className = "date";
-        // wrapper.appendChild(this.date);
         this.text = document.createElement('span');
-        // wrapper.appendChild(this.text);
 
         let style = document.createElement('style');
-        // debugger;
         style.textContent = `
             .wrapper {
                 display: inline-block;
@@ -30,19 +24,7 @@ class imageCard extends HTMLElement {
             .wrapper:hover {
                 background-color: #1B1B1E;
             }
-            // .wrapper:hover::after {
-            //     content: "${this.text.innerText}";//TODO:
-            //     position: fixed; 
-            //     top: 5px; left: 80%; right: 0; bottom: 5px;
-            //     z-index: 1; 
-            //     background: rgba(0,42,167,0.6); 
-            //     color: #fff; 
-            //     text-align: center; 
-            //     font-family: Arial, sans-serif; 
-            //     font-size: 11px; 
-            //     padding: 5px 10px; 
-            //     border: 1px solid #333; 
-            // }
+            
             .image {
                 max-width: 400px;
                 max-height: 400px;
@@ -69,41 +51,27 @@ class imageCard extends HTMLElement {
         });
         this.shadow.appendChild(style);
         this.shadow.appendChild(wrapper);
-        // debugger;
         this.onmouseenter = function (event) {
             this.picture.style.maxWidth = '500px';
-            this.text.style.display = 'block'; //
+            this.text.style.display = 'block';
             wrapper.appendChild(this.text);
-            // getComputedStyle(document.getElementsByTagName('style')[0], ':hover', '::after').content = this.getExif(
-            // 	this.picture
-            // );
-            TODO: /*this.text.innerHTML*/ this.text.innerHTML = this.getExif(this.picture); //'placeholder';TODO:
+            this.text.innerHTML = this.getExif(this.picture);
         };
 
         this.onmouseout = function (event) {
-            // wrapper.removeChild(this.text);
             this.text.style.display = 'none';
             this.picture.style.maxWidth = '400px';
         };
 
-        this.onclick = function (event) {
-            // wrapper.appendChild(this.date)
-            // this.date.innerText = response.uploaded;
-            // wrapper.style.width = '600px';
-            // wrapper.appendChild(this.text);
-        };
+        this.onclick = function (event) {};
 
         this.getExif = function (img) {
-            // debugger
             let allMetaDataSpan = this.text;
             EXIF.getData(img, function () {
-                // debugger; //
-                // let allMetaData = EXIF.getAllTags(this);
                 let Make = EXIF.getTag(this, 'Make');
                 let Model = EXIF.getTag(this, 'Model');
                 let PixelXDimension = EXIF.getTag(this, 'PixelXDimension');
                 let PixelYDimension = EXIF.getTag(this, 'PixelYDimension');
-
                 allMetaDataSpan.innerHTML = `Vendor: ${Make}; Model: ${Model}; Resolution: ${PixelXDimension}x${PixelYDimension}`; //JSON.stringify(allMetaData, null, '\t');
             });
             return allMetaDataSpan.innerText;
