9ad66e22b10399b9acee2779570985ed5c1e7189
Artur Zahorskyi <arthedza@gmail.com>
2019-04-03 16:32:30 +0300

    proj

--- index.js
+++ index.js
@@ -1,76 +1,179 @@
-import imageCard from "./modules/imageCard.js";
+/*jshint esversion: 8 */
+
+import imageCard from './modules/imageCard.js';
 customElements.define('image-card', imageCard);
+const sha1 = require('js-sha1');
+const EXIF = require('exif-js');
+
+const PORT = 3000;
+const contentWrapper = document.getElementsByClassName('content_wrapper')[0];
+const placeholder = document.getElementsByClassName('placeholder')[0];
+const btnRegister = document.getElementById('btn_register');
+const btnLogin = document.getElementById('btn_login');
+const email = document.getElementById('email'); //TODO: inputs verification
+const key = document.getElementById('password');
+
 
 function addElement(tagName, container) {
     return (container ? container : document.body).appendChild(document.createElement(tagName));
 }
 
-function getDate() {
-    return new Date().toLocaleString()
+function getCurrentDate() {
+    return new Date().toLocaleString();
 }
 
 function testFile(file) {
+    // debugger;
     //TODO: new func
+    //TODO: resolve + reject //TODO: IF?
+
     return new Promise(() => {
-        //TODO: resolve + reject //TODO: IF?
         return /jpeg|png|gif/gi.test(file.type) ?
-            (addElement('img').src = URL.createObjectURL(file)) : //TODO: separete function to insert image into the DOM
-            (addElement('p').innerText = 'Выбранный файл не является изображением'); //TODO: add img into web-component
+            addElement('image-card', contentWrapper).setAttribute('src', URL.createObjectURL(file)) //TODO: separete function to insert image into the DOM
+            :
+            alert('Выбранный файл не является изображением'); //TODO: add img into web-component
     });
 }
 
 //TODO: JSDOC /** */
-// const formData = new FormData();
 
 var selector = document.getElementById('image_selector');
 selector.onchange = function (event) {
+    // debugger
     for (var file of event.target.files) {
-        testFile(file).then(transport(file));
+        testFile(file).then(sendFile(file)); //TODO: get from coockies
+        // let metadata = getExif(file) //TODO: variable obj
     }
 };
 //TODO: rename funcs (function naming)
-async function transport(file) {
-    fetch('http://localhost:3000/Images/', {
+async function sendFile(file) {
+    // debugger;
+    fetch(`http://localhost:${PORT}/Images/`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                created: getDate(),
-                content: await encode(file)
+                uploaded: getCurrentDate(),
+                ovner: getUserName(email.value), //TODO://TODO://TODO://TODO://TODO:
+                // metadata: getExif(file),
+                content: await encodeFile(file)
             })
         })
         .then((response) => response.json())
         .then((response) => console.log(response));
 }
-//TODO: reject
-function encode(file) {
-    return new Promise((resolve, reject) => {
+
+function encodeFile(file) {
+    return new Promise((resolve) => {
         let reader = new FileReader();
         reader.readAsBinaryString(file);
         reader.onload = (event) => resolve(window.btoa(event.target.result));
     });
 }
-//TODO: headers and src (add to params) in a separate variables;
-//TODO: define quantity of loaded src
-function getImg(params) {
-    // let loadedData = {}
-    return fetch('http://localhost:3000/Images/').then(response => response.json()); //.then(response => console.log(response));
-    // return loadedData
-    // console.log()
+
+function getData(endpoint) {
+    return fetch(`http://localhost:${PORT}/${endpoint}/`).then((response) => response.json());
 }
 
 function showImg(img) {
-    return img.content ? addElement('image-card').setAttribute('src', `data:image/png;base64,${img.content}`) : null //= `data:image/png;base64,${img.content}` : null; //TODO: file types
+    placeholder.hidden = true;
+    return img.content ?
+        addElement('image-card', contentWrapper).setAttribute('src', `data:image/jpg;base64,${img.content}`) :
+        null;
 }
 
-window.onload = function (event) {
-    getImg().then(response => response.forEach(responseEntity => showImg(responseEntity))); //TODO: mb create custom response iterator
+function getUserName(loginMail) {
+    return (function () {
+        return loginMail.split('@')[0];
+    })();
+}
+
+function addNewUser(loginMail, key) {
+    fetch(`http://localhost:${PORT}/Users/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                [getUserName(loginMail)]: {
+                    loginMail,
+                    name: getUserName(loginMail),
+                    key,
+                    registered: getCurrentDate()
+                }
+            })
+        })
+        .then((response) => response.json())
+        .then((response) => alert('registered'));
+}
+//TODO: async/await like here
+async function testUser(userName, key) {
+    let response = await getData('Users');
+    // debugger
+    let user = await response.find((user) => user[userName]); //TODO: get data with username-endpoints//TODO://TODO://TODO://TODO://TODO://TODO://TODO://TODO://TODO://TODO://TODO://TODO:
+    // return await response[userName] ? true : false
+    //TODO: email verification: mail domains
+    // debugger
+    if (!user) {
+        alert('user does not exist');
+        location.reload(); //TODO:
+    } else
+        user[userName].name === userName && user[userName].key === key ?
+        beginSession(key) /*.bind(null, await user[userName], key)*/ :
+        console.log('lol');
+}
+
+function deleteCoockie(name) {
+    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+function beginSession(key) {
+    console.log('hi');
+    let logOut = addElement('button', document.getElementsByClassName('topbar_wrapper')[0]); //TODO: make it const
+    logOut.innerText = 'Logout';
+    logOut.onclick = function (event) {
+        alert('logout');
+        deleteCoockie('key');
+        location.reload();
+    };
+    // debugger
+    // document.getElementById('image_selector').style.display = 'inline-block'
+    document.cookie = `key=${key};max-age=3600`; //TODO://TODO://TODO://TODO://TODO:
+    getData('Images').then((response) =>
+        response.forEach((responseEntity) => {
+            // debugger
+            // placeholder.hidden = true;
+            responseEntity.ovner === getUserName(email.value) ? showImg(responseEntity) : placeholder.hidden = false; //null; TODO:
+        })
+    ); //TODO: mb create custom response iterator
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+}
+
+window.onload = function (event) {};
+
+btnRegister.onclick = (event) => addNewUser(email.value, sha1(key.value)); //TODO: inputs verification
+
+btnLogin.onclick = function (event) {
+    // debugger
+    testUser(getUserName(email.value), sha1(key.value));
+    document.getElementById('label').style.display = 'block';
+    document.getElementById('auth').style.display = 'none';
 };
 
 //TODO:
-Array.from(document.getElementsByTagName('image-card')).forEach(element => element.addEventListener('mouseover', function (event) {
-    event.target.style = `
-        background-color: green;
-    `
-}))
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+function getExif(img) {
+
+
+    EXIF.getData(img, function () {
+        var allMetaData = EXIF.getAllTags(this);
+        var allMetaDataSpan = document.getElementById("allMetaDataSpan");
+        allMetaDataSpan.innerHTML = JSON.stringify(allMetaData, null, "\t");
+    });
+}
+
+// }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
--- modules/imageCard.js
+++ modules/imageCard.js
@@ -1,30 +1,67 @@
+/*jshint esversion: 8 */
+const EXIF = require('exif-js');
 class imageCard extends HTMLElement {
     constructor() {
         super();
+        // let text;
+
         let wrapper = document.createElement('div');
-        wrapper.className = "wrapper";
+        wrapper.className = 'wrapper';
         this.picture = document.createElement('img');
-        this.picture.className = "image";
+        this.picture.className = 'image';
         wrapper.appendChild(this.picture);
-        this.date = document.createElement('span');
-        this.date.className = "date";
-        wrapper.appendChild(this.date);
+        // this.date = document.createElement('span');
+        // this.date.className = "date";
+        // wrapper.appendChild(this.date);
+        this.text = document.createElement('span');
+        // wrapper.appendChild(this.text);
 
         let style = document.createElement('style');
+        // debugger;
         style.textContent = `
             .wrapper {
                 display: inline-block;
+                flex-direction: row-reverse;
+                flex-wrap: wrap;
+                align-items: flex-end;
+                align-content: space-between;
                 margin: 20px;
             }
             .wrapper:hover {
-                // z-index: 3;
-                background-color: red;
+                background-color: #1B1B1E;
             }
+            // .wrapper:hover::after {
+            //     content: "${this.text.innerText}";//TODO:
+            //     position: fixed; 
+            //     top: 5px; left: 80%; right: 0; bottom: 5px;
+            //     z-index: 1; 
+            //     background: rgba(0,42,167,0.6); 
+            //     color: #fff; 
+            //     text-align: center; 
+            //     font-family: Arial, sans-serif; 
+            //     font-size: 11px; 
+            //     padding: 5px 10px; 
+            //     border: 1px solid #333; 
+            // }
             .image {
                 max-width: 400px;
                 max-height: 400px;
-                border: dotted 1px #555;
-                transition: all 1s;
+                border: solid 1px #555;
+                transition: 0.4s;
+                // float: left;
+            }
+            span {
+                // position: fixed;
+                // width: 100%;
+                display: none;
+                // top: 50%;
+                // left: 50%;
+                // bottom: 50%;
+                font-size: 14px;
+                font-family: sans-serif;
+                color: white;
+                // transform: translate(-50%,-50%);
+                transition: 0.4s;
             }
         `;
         this.shadow = this.attachShadow({
@@ -32,10 +69,50 @@ class imageCard extends HTMLElement {
         });
         this.shadow.appendChild(style);
         this.shadow.appendChild(wrapper);
+        // debugger;
+        this.onmouseenter = function (event) {
+            this.picture.style.maxWidth = '500px';
+            this.text.style.display = 'block'; //
+            wrapper.appendChild(this.text);
+            // getComputedStyle(document.getElementsByTagName('style')[0], ':hover', '::after').content = this.getExif(
+            // 	this.picture
+            // );
+            TODO: /*this.text.innerHTML*/ this.text.innerHTML = this.getExif(this.picture); //'placeholder';TODO:
+        };
+
+        this.onmouseout = function (event) {
+            // wrapper.removeChild(this.text);
+            this.text.style.display = 'none';
+            this.picture.style.maxWidth = '400px';
+        };
+
+        this.onclick = function (event) {
+            // wrapper.appendChild(this.date)
+            // this.date.innerText = response.uploaded;
+            // wrapper.style.width = '600px';
+            // wrapper.appendChild(this.text);
+        };
+
+        this.getExif = function (img) {
+            // debugger
+            let allMetaDataSpan = this.text;
+            EXIF.getData(img, function () {
+                // debugger; //
+                // let allMetaData = EXIF.getAllTags(this);
+                let Make = EXIF.getTag(this, 'Make');
+                let Model = EXIF.getTag(this, 'Model');
+                let PixelXDimension = EXIF.getTag(this, 'PixelXDimension');
+                let PixelYDimension = EXIF.getTag(this, 'PixelYDimension');
+
+                allMetaDataSpan.innerHTML = `Vendor: ${Make}; Model: ${Model}; Resolution: ${PixelXDimension}x${PixelYDimension}`; //JSON.stringify(allMetaData, null, '\t');
+            });
+            return allMetaDataSpan.innerText;
+        };
     }
     static get observedAttributes() {
-        return ["src"];
+        return ['src'];
     }
+
     attributeChangedCallback(attrName, oldVal, newVal) {
         this.picture.src = this.getAttribute('src');
     }
